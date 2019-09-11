d725b1f81c5968db129b9e1ecf472fbe1df16093
hendryboyz <hendry.js1247@gmail.com>
2019-09-11 16:12:50 +0800

    Setup Webpack with Babel
    To transpile the unsupported ECMAScript to vanilla Javascript

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+import "./another";
+
 console.log('Hello Project.');
 
 document.getElementById("my-button").addEventListener("click", function() {
   console.log("I'm clicked!");
+  myFunction();
 });
+
+var myFunction = () => {
+  console.log("my-function");
+};
