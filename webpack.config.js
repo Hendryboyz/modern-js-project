e33553f22d12d071b28023fc2b84b5ae1a5b96b3
hendryboyz <hendry.js1247@gmail.com>
2019-09-17 09:40:09 +0800

    Introduce webpack-plugin to clean and auto gen every Webpack build
    clean-webpack-plugin: clean dist/ directory
    html-webpack-plugin: generate index.html to dist/ directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,37 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader']
-            }
-        ]
-    },
-    resolve: {
-    	extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
     ],
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    }
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Hello index.html by HtmlWebpackPlugin',
+      template: './src/index.template.html',
+    }),
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
 };
