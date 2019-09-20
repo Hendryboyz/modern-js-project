814af4871f80fb1a0d47f9df46f9755d7c21ecd9
hendryboyz <hendry.js1247@gmail.com>
2019-09-20 09:37:57 +0800

    Apply dotenv package to build from external env file

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -1,13 +1,12 @@
 const { DefinePlugin } = require('webpack');
+const Dotnev = require('dotenv-webpack');
 
 module.exports = {
   mode: 'development',
   devtool: 'eval-source-map',
   plugins: [
-    new DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('development'),
-      }
+    new Dotnev({
+      path: './.env.development'
     }),
   ],
 };
